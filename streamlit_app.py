a72a8a639f07a096bd350873576565e5bc9f322a
Beatrice Pscheidl <ds21m010@technikum-wien.ac.at>
2022-06-04 19:21:41 +0200

    read tweets

--- streamlit_app.py
+++ streamlit_app.py
@@ -34,37 +34,50 @@ def init_connection():
 conn = init_connection()
 
 # Perform query.
-# Uses st.experimental_memo to only rerun when the query changes or after 10 min.
-@st.experimental_memo(ttl=600)
+# Uses st.experimental_memo to only rerun when the query changes or after 10 secs.
+@st.experimental_memo(ttl=10)
 def run_query(query):
     with conn.cursor() as cur:
         cur.execute(query)
         return cur.fetchall()
 
-rows = run_query("SELECT top 10 * from twitter_api.HashtagAggregations;")
+counts = run_query("SELECT top 10 * from twitter_api.HashtagAggregations;")
+tweets = run_query("SELECT top 10 * from twitter_api.TweetText")
+
+
+############ Display page content ############
+st.header('Big Data Project Dashboard (Twitter Use Case)')
+
+# Print latest tweets
+for row in tweets:
+    st.write(f"Tag:{row[2]}, Tweet:{row[0]}")
+
 
 # Print results.
-for row in rows:
-    st.write(f"{row[0]} has a :{row[1]}:")
+for row in counts:
+    st.write(f"Tag {row[0]} had {row[1]} tweets in the last minute!")
+
+############ End page content ############
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+# with st.echo(code_location='below'):
+#     total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
+#     num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
 
-    points_per_turn = total_points / num_turns
+#     Point = namedtuple('Point', 'x y')
+#     data = []
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+#     points_per_turn = total_points / num_turns
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+#     for curr_point_num in range(total_points):
+#         curr_turn, i = divmod(curr_point_num, points_per_turn)
+#         angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
+#         radius = curr_point_num / total_points
+#         x = radius * math.cos(angle)
+#         y = radius * math.sin(angle)
+#         data.append(Point(x, y))
+
+#     st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
+#         .mark_circle(color='#0068c9', opacity=0.5)
+#         .encode(x='x:Q', y='y:Q'))
